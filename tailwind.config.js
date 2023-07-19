b579f1d852d78ebc0ac15ff31b2b927753c4a726
jay-rt <thapa.jayrt@gmail.com>
2023-07-19 01:22:34 -0400

    worked on home page and added some generic components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-];
-
-export const theme = {
-  extend: {
-    fontFamily: {
-      satoshi: ["Satoshi", "sans-serif"],
-      inter: ["Inter", "sans-serif"],
-    },
-    colors: {
-      "primary-orange": "#FF5722",
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-orange": "#FF5722",
+      },
     },
   },
+  plugins: [],
 };
-
-export const plugins = [];
