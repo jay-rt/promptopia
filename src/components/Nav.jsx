793d8ad4350648c5839702dfb5c4d3ab0560e525
jay-rt <thapa.jayrt@gmail.com>
2023-07-19 22:58:22 -0400

    added navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,132 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [providers, setProviders] = useState(null);
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  useEffect(() => {
+    const getSetProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+    getSetProviders();
+  }, []);
+
+  const isUserLoggedIn = true;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+
+      {/** Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-post" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" className="outline_btn" onClick={signOut}>
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/** Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              alt="profile"
+              width={37}
+              height={37}
+              className="rounded-full"
+              onClick={() => setMenuToggle((prev) => !prev)}
+            />
+            {menuToggle && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setMenuToggle(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setMenuToggle(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  className="black_btn w-full mt-5"
+                  onClick={() => {
+                    setMenuToggle(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
